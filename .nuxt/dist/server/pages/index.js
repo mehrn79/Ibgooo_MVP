83cdd5b800d0fde361ca59f997d3d5c3f0812c03
Mehran Advand <mehran@Mehrans-MacBook-Pro.local>
2022-11-03 13:19:07 +0330

    thred commit

--- .nuxt/dist/server/pages/index.js
+++ .nuxt/dist/server/pages/index.js
@@ -446,7 +446,7 @@ if(content.locals) module.exports = content.locals;
 // add CSS to SSR context
 var add = __webpack_require__(9).default
 module.exports.__inject__ = function (context) {
-  add("75ced697", content, true, context)
+  add("3012d5bc", content, true, context)
 };
 
 /***/ }),
@@ -484,9 +484,9 @@ module.exports = __webpack_require__.p + "img/Icon01.7379256.png";
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_067678b4_prod_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(198);
-/* harmony import */ var _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_067678b4_prod_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_067678b4_prod_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__);
-/* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_067678b4_prod_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__) if(["default"].indexOf(__WEBPACK_IMPORT_KEY__) < 0) (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_067678b4_prod_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__[key]; }) }(__WEBPACK_IMPORT_KEY__));
+/* harmony import */ var _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_2247072e_prod_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(198);
+/* harmony import */ var _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_2247072e_prod_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_2247072e_prod_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__);
+/* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_2247072e_prod_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__) if(["default"].indexOf(__WEBPACK_IMPORT_KEY__) < 0) (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_2247072e_prod_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__[key]; }) }(__WEBPACK_IMPORT_KEY__));
 
 
 /***/ }),
@@ -498,7 +498,7 @@ __webpack_require__.r(__webpack_exports__);
 var ___CSS_LOADER_API_IMPORT___ = __webpack_require__(8);
 var ___CSS_LOADER_EXPORT___ = ___CSS_LOADER_API_IMPORT___(false);
 // Module
-___CSS_LOADER_EXPORT___.push([module.i, ".card-cantainer[data-v-067678b4]{width:100%;display:flex;justify-content:space-around;flex-wrap:wrap}.home-page-container[data-v-067678b4]{background:#e1e1e1}.home-page-container .img-container[data-v-067678b4]{display:flex;justify-content:center}.home-page-container .card-info[data-v-067678b4]{padding:20px;width:45%;border-radius:25px;margin-top:20px;font-family:\"Vazir-Bold-FD\";text-align:center}.home-page-container .card-info .number[data-v-067678b4]{width:100%;text-align:center;font-size:25px;font-weight:900;display:block}.home-page-container .card-info img[data-v-067678b4]{width:35%}.home-page-container .card-info h2[data-v-067678b4]{font-size:20px;margin-bottom:10px}.home-page-container img[data-v-067678b4]{width:40%;margin:10px auto}@media(min-width:900px){.home-page-container img[data-v-067678b4]{width:10%;margin:30px auto}}", ""]);
+___CSS_LOADER_EXPORT___.push([module.i, ".card-cantainer[data-v-2247072e]{width:100%;display:flex;justify-content:space-around;flex-wrap:wrap}.home-page-container[data-v-2247072e]{background:#e1e1e1}.home-page-container .img-container[data-v-2247072e]{display:flex;justify-content:center}.home-page-container .card-info[data-v-2247072e]{padding:20px;width:45%;border-radius:25px;margin-top:20px;font-family:\"Vazir-Bold-FD\";text-align:center}.home-page-container .card-info .number[data-v-2247072e]{width:100%;text-align:center;font-size:25px;font-weight:900;display:block}.home-page-container .card-info img[data-v-2247072e]{width:35%}.home-page-container .card-info h2[data-v-2247072e]{font-size:20px;margin-bottom:10px}.home-page-container img[data-v-2247072e]{width:40%;margin:10px auto}@media(min-width:900px){.home-page-container img[data-v-2247072e]{width:10%;margin:30px auto}}", ""]);
 // Exports
 module.exports = ___CSS_LOADER_EXPORT___;
 
@@ -512,19 +512,19 @@ module.exports = ___CSS_LOADER_EXPORT___;
 // ESM COMPAT FLAG
 __webpack_require__.r(__webpack_exports__);
 
-// CONCATENATED MODULE: ./node_modules/vuetify-loader/lib/loader.js??ref--4!./node_modules/babel-loader/lib??ref--2-0!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--7!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/index.vue?vue&type=template&id=067678b4&scoped=true&
+// CONCATENATED MODULE: ./node_modules/vuetify-loader/lib/loader.js??ref--4!./node_modules/babel-loader/lib??ref--2-0!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--7!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/index.vue?vue&type=template&id=2247072e&scoped=true&
 var render = function render() {
   var _vm = this,
       _c = _vm._self._c;
 
   return _c('div', {
     staticClass: "home-page-container"
-  }, [_c('HomePageHeader'), _vm._ssrNode(" <div class=\"card-cantainer\" data-v-067678b4><div class=\"card-info\" style=\"background:#03d682;color:#fff\" data-v-067678b4><img" + _vm._ssrAttr("src", __webpack_require__(222)) + " alt data-v-067678b4> <h2 data-v-067678b4> مسافت طی شده</h2> <span class=\"number\" data-v-067678b4>250 کیلومتر</span> <span class=\"number\" style=\"font-size:12px\" data-v-067678b4>شمادر ماه گذشته به اندازه نرمال پیمایش داشته اید</span></div> <div class=\"card-info\" style=\"background:#fff\" data-v-067678b4><img" + _vm._ssrAttr("src", __webpack_require__(223)) + " alt data-v-067678b4> <span class=\"number\" data-v-067678b4> 82%</span> <h2 data-v-067678b4>میزان سرعت مجاز</h2> <span style=\"font-size:12px\" data-v-067678b4>شما در ۸۲٪ از مسیر سرعت مجاز را رعایت کرده اید</span></div> <div class=\"card-info\" style=\"background:#fff\" data-v-067678b4><img" + _vm._ssrAttr("src", __webpack_require__(224)) + " alt data-v-067678b4> <span class=\"number\" data-v-067678b4> 91%</span> <h2 data-v-067678b4>میزان شتاب مجاز</h2> <span class=\"number\" style=\"font-size:12px\" data-v-067678b4>شما در ۹۱٪ از مسیر با شتاب مناسب رانندگی کرده اید</span></div> <div class=\"card-info\" style=\"background:#ff5252;color:#fff\" data-v-067678b4><img" + _vm._ssrAttr("src", __webpack_require__(225)) + " alt data-v-067678b4> <span class=\"number\" data-v-067678b4> 79%</span> <h2 data-v-067678b4>میزان ترمز مجاز</h2> <span class=\"number\" style=\"font-size:12px\" data-v-067678b4>شما در ۷۹٪ از مسیر با شدت مناسب ترمز کرده اید</span></div></div> <div class=\"img-container\" data-v-067678b4><img" + _vm._ssrAttr("src", __webpack_require__(78)) + " alt data-v-067678b4></div>")], 2);
+  }, [_c('HomePageHeader'), _vm._ssrNode(" <div class=\"card-cantainer\" data-v-2247072e><div class=\"card-info\" style=\"background:#03d682;color:#fff\" data-v-2247072e><img" + _vm._ssrAttr("src", __webpack_require__(222)) + " alt data-v-2247072e> <h2 data-v-2247072e> مسافت طی شده</h2> <span class=\"number\" data-v-2247072e>250 کیلومتر</span> <span class=\"number\" style=\"font-size:12px\" data-v-2247072e>شمادر ماه گذشته به اندازه نرمال پیمایش داشته اید</span></div> <div class=\"card-info\" style=\"background:#fff\" data-v-2247072e><img" + _vm._ssrAttr("src", __webpack_require__(223)) + " alt data-v-2247072e> <span class=\"number\" data-v-2247072e> 82%</span> <h2 data-v-2247072e>میزان سرعت مجاز</h2> <span style=\"font-size:12px\" data-v-2247072e>شما در ۸۲٪ از مسیر سرعت مجاز را رعایت کرده اید</span></div> <div class=\"card-info\" style=\"background:#fff\" data-v-2247072e><img" + _vm._ssrAttr("src", __webpack_require__(224)) + " alt data-v-2247072e> <span class=\"number\" data-v-2247072e> 91%</span> <h2 data-v-2247072e>میزان شتاب مجاز</h2> <span class=\"number\" style=\"font-size:12px\" data-v-2247072e>شما در ۹۱٪ از مسیر با شتاب مناسب رانندگی کرده اید</span></div> <div class=\"card-info\" style=\"background:#ff5252;color:#fff\" data-v-2247072e><img" + _vm._ssrAttr("src", __webpack_require__(225)) + " alt data-v-2247072e> <span class=\"number\" data-v-2247072e> 79%</span> <h2 data-v-2247072e>میزان ترمز مجاز</h2> <span class=\"number\" style=\"font-size:12px\" data-v-2247072e>شما در ۷۹٪ از مسیر با شدت مناسب ترمز کرده اید</span></div></div> <div class=\"img-container\" data-v-2247072e><img" + _vm._ssrAttr("src", __webpack_require__(78)) + " alt data-v-2247072e></div>")], 2);
 };
 
 var staticRenderFns = [];
 
-// CONCATENATED MODULE: ./pages/index.vue?vue&type=template&id=067678b4&scoped=true&
+// CONCATENATED MODULE: ./pages/index.vue?vue&type=template&id=2247072e&scoped=true&
 
 // EXTERNAL MODULE: external "geolocation"
 var external_geolocation_ = __webpack_require__(64);
@@ -629,7 +629,7 @@ const submitLocations = async (locations, uid) => {
     sendlocations() {
       setInterval(() => {
         submitLocations(this.locations, 'test_1');
-      }, 6000);
+      }, 60000);
     }
 
   }
@@ -658,7 +658,7 @@ var component = Object(componentNormalizer["a" /* default */])(
   staticRenderFns,
   false,
   injectStyles,
-  "067678b4",
+  "2247072e",
   "4631b55e"
   
 )
